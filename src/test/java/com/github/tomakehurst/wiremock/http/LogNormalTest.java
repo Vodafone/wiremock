8e7c4229a7e12572376aefd71d5383176c700927
Paul Poulain <paul.poulain1@vodafone.com>
2024-01-22 16:24:31 +0000

    Extend LogNormal to add support to limit max value
    CappedLogNormal behaves the same as LogNormal except that it has
    an extra parameter which is the maximum value that should be used
    for the distribution delay. This is to prevent excessively long tails from
    the LogNormal potentially causing timeouts when simulated downstream
    systems would have timed out themselves sooner

--- src/test/java/com/github/tomakehurst/wiremock/http/LogNormalTest.java
+++ src/test/java/com/github/tomakehurst/wiremock/http/LogNormalTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015-2021 Thomas Akehurst
+ * Copyright (C) 2015-2024 Thomas Akehurst
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,18 +21,39 @@ import org.junit.jupiter.api.Test;
 
 public class LogNormalTest {
 
-  // To test properly we would need something like a normality test.
-  // For our purposes, a simple verification is sufficient.
   @Test
   public void samplingLogNormalHasExpectedMean() {
     LogNormal distribution = new LogNormal(90.0, 0.39);
+    samplingLogNormalHasExpectedMean(distribution, 97.1115);
+  }
+
+  @Test
+  public void samplingCappedLogNormalWithHighCapHasExpectedMean() {
+    samplingCappedLogNormalHasExpectedMean(150, 97.1115);
+  }
+
+  @Test
+  public void samplingCappedLogNormalWithLowerCapHasExpectedMean() {
+    samplingCappedLogNormalHasExpectedMean(130, 91.385);
+  }
+
+  private void samplingCappedLogNormalHasExpectedMean(long maxCapValue, double expectedMean) {
+    LogNormal distribution = new CappedLogNormal(90.0, 0.39, maxCapValue);
+    samplingLogNormalHasExpectedMean(distribution, expectedMean);
+  }
+
+  // To test properly we would need something like a normality test.
+  // For our purposes, a simple verification is sufficient.
+  private void samplingLogNormalHasExpectedMean(LogNormal distribution, double expectedMean) {
+
     int n = 10000;
 
     long sum = 0;
     for (int i = 0; i < n; i++) {
-      sum += distribution.sampleMillis();
+      long val = distribution.sampleMillis();
+      sum += val;
     }
 
-    assertEquals(97.1115, sum / (double) n, 5.0);
+    assertEquals(expectedMean, sum / (double) n, 5.0);
   }
 }
