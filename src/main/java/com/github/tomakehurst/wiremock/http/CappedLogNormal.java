9544dbadc5bf1dce70036ad57c33c9839b2b8688
Paul Poulain <paul.poulain1@vodafone.com>
2024-02-01 15:10:06 +0000

    Change to resample if the generated value is greater than the max rather than capping to the max value.

--- src/main/java/com/github/tomakehurst/wiremock/http/CappedLogNormal.java
+++ src/main/java/com/github/tomakehurst/wiremock/http/CappedLogNormal.java
@@ -19,14 +19,11 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 /**
- * Returns log normally distributed values. Takes two parameters, the median (50th percentile) of
- * the lognormal and the standard deviation of the underlying normal distribution.
+ * Returns log normally distributed values, capped at a maximum value to eliminate long tales. Takes three
+ * parameters, the median (50th percentile) of the lognormal, the standard deviation of the underlying
+ * normal distribution and the maximum value at which to cap the response delay.
  *
- * <p>The larger the standard deviation the longer the tails.
- *
- * @see <a
- *     href="https://www.wolframalpha.com/input/?i=lognormaldistribution%28log%2890%29%2C+0.1%29">lognormal
- *     example</a>
+ * @see <a href="https://www.wolframalpha.com/input/?i=lognormaldistribution%28log%2890%29%2C+0.1%29">lognormal example</a>
  */
 public class CappedLogNormal extends LogNormal {
 
@@ -44,10 +41,25 @@ public class CappedLogNormal extends LogNormal {
       @JsonProperty("maxValue") double maxValue) {
     super(median, sigma);
     this.maxValue = maxValue;
+
+    if (maxValue < median) {
+      throw new IllegalArgumentException("The max value has to be at least greater than the median");
+    }
   }
 
   @Override
   public long sampleMillis() {
-    return Math.round(Math.min(maxValue, super.sampleMillis()));
+
+    long generatedValue = super.sampleMillis();
+
+    // Rather than capping the value at the max, if it's over the max value, then resample, but only do that a few times
+    int i = 0;
+    while (generatedValue > maxValue && i < 10) {
+      generatedValue = super.sampleMillis();
+      i++;
+    }
+
+    // Belt and braces, in the unlikely event the generated value is still over the max, cap it at the max
+    return Math.round(Math.min(maxValue, generatedValue));
   }
 }
